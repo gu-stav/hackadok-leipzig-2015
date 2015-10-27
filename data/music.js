d588c6ba6da424a74a17bb2a2e2acfe1520399f6
Gustav Pursche <pursche@posteo.de>
2015-10-27 12:14:17 +0100

    Add more music

--- data/music.js
+++ data/music.js
@@ -18,15 +18,22 @@ define(function() {
       'Tom Waits',
       'Jeff Buckley',
       'Crystall Fighters',
-
     ],
 
     [
       'Wolfgang Amadeus Mozart',
+      'K.D. Lang',
+      'The Unthanks',
+      'Wieb Zigtema',
+      'Charlie Marshell',
     ],
 
     [
       'Folk musik',
+      'Damien Rice',
+      'Eddie Vedder',
+      'Arcade Fire',
+      'Woodkid',
     ],
 
     [
@@ -47,11 +54,13 @@ define(function() {
     [
       'Weiss',
       'FIO',
+      'Alister Spence Trio',
     ],
 
     [
       'ICAN SWIM',
       'Patti Smith',
+      'Cat Power',
     ],
 
     [
@@ -67,11 +76,14 @@ define(function() {
       'Dillon',
       'Say Lou Lou',
       'Justin Bieber',
-
     ],
 
     [
       'Abba',
+      'Savages',
+      'The National',
+      'Sophie Hunger',
+      'Bettie Serveert',
     ],
 
     [
@@ -88,7 +100,15 @@ define(function() {
     ],
 
     [
-      'Tekno',
+      'Jeff Buckley',
+      'Tender Forever',
+      'Broken Social Scene',
+      'Laura Marling',
+      'Coco Rosie',
+      'Scud Mountain Boys',
+      'Jens Lekman',
+      'Caroline Kennedy Conspiracy',
+      'Lucinda Williams',
     ],
 
     [
@@ -99,14 +119,25 @@ define(function() {
 
     [
       'Charlotte Gainsbourg',
+      'Linnea Olsson',
+      'La Pat',
+      'Grizzly Bear',
+      'Ane Brun',
+      'Sia',
+      'Kyrie Kristmanson',
     ],
 
     [
       'Joan Jett and the Blackhearts',
+      'Firekites',
+      'KES Band',
+      'Here We Go Magic',
     ],
 
     [
       'Nick Cave',
+      'Andreas Scholl',
+      'Laurie Anderson',
     ],
 
     [
@@ -116,6 +147,7 @@ define(function() {
     [
       'John Coltrain',
       'Nat King Cole',
+      'Zola Jesus',
     ],
 
     [
@@ -123,10 +155,226 @@ define(function() {
       'Adelle',
       'Pharel Williams',
       'Taylor Swift',
+      'Jenny Hval',
     ],
 
     [
       'Pharel Williams',
+      'PJ Harvey',
+      'Radiohead',
+      'The Mumps',
+      'The Desks',
+      'Mountain Man',
+      'Laura Jean',
+     ],
+
+    [
+      'Schahe ghajur-o-mehrabane ma',
+      'Gott erhalte Franz den Kaiser',
+      'Dhafer Youssef',
+      'Mama Sadio',
+      'Ityopp\'ya, Ityopp\'ya, Ityopp\'ya qida mi',
+      'Oumou Sangare',
+      'The Cure',
+      'Frank Zappa',
+      'A Portuguesa',
+      'The Long Run',
+    ],
+
+    [
+      'Emotional Rescue',
+      'Goldroom',
+      'Against the Wind',
+      'Tom Waits',
+      'Jeff Buckley',
+      'Crystall Fighters',
+      'Aarhus Pianofabrik',
+      'Barthol',
+      'Chavanne',
+      'Cherny',
+    ],
+
+    [
+      'Ellington',
+      'Wolfgang Amadeus Mozart',
+      'K.D. Lang',
+      'The Unthanks',
+      'Chase',
+      'Chassaigne',
+      'Wieb Zigtema',
+      'Charlie Marshell',
+    ],
+
+    [
+      'Gulbransen',
+      'Folk musik',
+      'Knight',
+      'Mozart piano company',
+      'Damien Rice',
+      'Eddie Vedder',
+      'Arcade Fire',
+      'Woodkid',
+    ],
+
+    [
+      'Hoodoo Revelator',
+      'Methew Welch Bagpipes',
+      'Fruit Flies',
+      'Michael McDonald',
+      'George Michael',
+      'Gary Morris',
+      'John Schneider',
+      'The breath Colevctive',
+      'John Anderson',
+      'Jason Mraz',
+    ],
+
+    [
+      'The Box Tops',
+      'Boy George',
+      'The Fantasy Orchestra - Bristol',
+      'Julio Delaquaff Landscape',
+      'Pussy Riot',
+      'David Byrne',
+      'Candy Darling',
+    ],
+
+    [
+      'David Bowie',
+      'Nawal Al Zoghbi',
+      'Weiss',
+      'FIO',
+      'Alister Spence Trio',
+    ],
+
+    [
+      'ICAN SWIM',
+      'Patti Smith',
+      'Cat Power',
+      'Toni QattanWael Kfoury',
+      'Rabeh Sager',
+    ],
+
+    [
+      'Orquestra DiVersiones',
+      'Medir',
+      'La Moral Distreta',
+      'Els Cremats',
+    ],
+
+    [
+      'St Vincent',
+      'Savages',
+      'Dillon',
+      'Darine Hadchiti',
+      'Farid El Atrache',
+      'Ali El Haggar',
+      'Say Lou Lou',
+      'Justin Bieber',
+    ],
+
+    [
+      'The National',
+      'Sophie Hunger',
+      'Bettie Serveert',
+      'Abba',
+      'Savages',
+    ],
+
+    [
+      'CocoRosie',
+      'Bonobo',
+      'Caracal',
+      'Washed Out',
+      'Born In the Echoes',
+      'Safari','Keep On The Grass Folk Music',
+      'Konobo',
+      'The Saintbox',
+      'Dagadana',
+    ],
+
+    [
+      'Bullet for My Valentine',
+      'Laura Marling',
+      'Coco Rosie',
+      'Scud Mountain Boys',
+      'Jens Lekman',
+      'Caroline Kennedy Conspiracy',
+      'Lucinda Williams',
+    ],
+
+    [
+      'ČAČI VORBA',
+      'Asak Balkan Trio',
+      'Clutch',
+      'Lady Gaga',
+      'Alter Bridge',
+    ],
+
+    [
+      'Charlotte Gainsbourg',
+      'Linnea Olsson',
+      'La Pat',
+      'Grizzly Bear',
+      'Ane Brun',
+      'Sia',
+      'Mutual Mortuary',
+      'Kyrie Kristmanson',
+      'Skinny Puppy',
+    ],
+
+    [
+      'Joan Jett and the Blackhearts',
+      'Firekites',
+      'KES Band',
+      'Here We Go Magic',
+    ],
+
+    [
+      'Nick Cave',
+      'Andreas Scholl',
+      'John Conlee',
+      'Eddie Rabbitt',
+      'Years After You',
+      'Lacy J. Dalton',
+      'Ricky Skaggs',
+      'Laurie Anderson',
+    ],
+
+    [
+      'No music',
+    ],
+
+    [
+      'John Coltrain',
+      'Earl Thomas Conley',
+      'John Coltrain',
+      'Nobody Wants to Be Alone',
+      'John Coltrain',
+      'Crystal Gayle',
+      'A World Without Love',
+      'Old Hippie',
+      'The Bellamy Brothers',
+      'Nat King Cole',
+      'Zola Jesus',
+    ],
+
+    [
+      'Justin Bieber',
+      'Adelle',
+      'Pharel Williams',
+      'Taylor Swift',
+      'Jenny Hval',
+    ],
+
+    [
+      'Pharel Williams',
+      'PJ Harvey',
+      'Radiohead',
+      'The Mumps',
+      'The Desks',
+      'Mountain Man',
+      'Laura Jean',
     ]
   ];
 });
